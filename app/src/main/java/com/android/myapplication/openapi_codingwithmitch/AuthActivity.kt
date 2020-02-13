d299b6b4ce30c8a03b2bab94ecaaca5dfae68682
Hady Salhab <hadysalhab1@gmail.com>
2020-02-13 15:17:40 +1100

    feat: starting point

---
+++ app/src/main/java/com/android/myapplication/openapi_codingwithmitch/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.android.myapplication.openapi_codingwithmitch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
