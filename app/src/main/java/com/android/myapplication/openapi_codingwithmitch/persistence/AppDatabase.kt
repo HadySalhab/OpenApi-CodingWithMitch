e2bb293e07771d3686490bc642fa905d963d2ceb
Hady Salhab <hadysalhab1@gmail.com>
2020-02-13 17:10:49 +1100

    Room persistence setup

--- app/src/main/java/com/android/myapplication/openapi_codingwithmitch/persistence/AppDatabase.kt
+++ app/src/main/java/com/android/myapplication/openapi_codingwithmitch/persistence/AppDatabase.kt
@@ -1,5 +1,15 @@
 package com.android.myapplication.openapi_codingwithmitch.persistence
 
-abstract class AppDatabase{
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.android.myapplication.openapi_codingwithmitch.models.AccountProperties
+import com.android.myapplication.openapi_codingwithmitch.models.AuthToken
 
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1, exportSchema = true)
+abstract class AppDatabase :RoomDatabase() {
+    abstract fun getAuthTokenDao(): AuthTokenDao
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+    companion object{
+        const val DATABASE_NAME = "app_db"
+    }
 }
--- app/src/main/java/com/android/myapplication/openapi_codingwithmitch/persistence/AuthTokenDao.kt
+++ app/src/main/java/com/android/myapplication/openapi_codingwithmitch/persistence/AuthTokenDao.kt
@@ -1,5 +1,20 @@
 package com.android.myapplication.openapi_codingwithmitch.persistence
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.android.myapplication.openapi_codingwithmitch.models.AuthToken
+
+@Dao
 interface AuthTokenDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(authToken: AuthToken):Long
+
+    //when we logout
+    @Query("UPDATE auth_token SET token=null WHERE account_pk = :pk")
+    fun nullifyToken(pk:Int):Int
+
 
 }
